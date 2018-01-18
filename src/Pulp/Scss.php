d6affbf8ee92acf020fe998879f9879668fe4672
Mark Kimsal <metrofindings@gmail.com>
2018-01-18 15:56:06 -0500

    Add importPath option

--- src/Pulp/Scss.php
+++ src/Pulp/Scss.php
@@ -9,7 +9,20 @@ class Scss extends DataPipe {
 	public $parser;
 
 	public function __construct($opts) {
-		$this->parser = new \Leafo\Scss\Compiler();
+		$this->parser = new \Leafo\ScssPhp\Compiler();
+		if (array_key_exists('importPath', $opts)) {
+			$importPathList = $opts['importPath'];
+		}
+		if (array_key_exists('importPathList', $opts)) {
+			$importPathList = $opts['importPathList'];
+		}
+		if (!is_array($importPathList)) {
+			$importPathList = array($importPathList);
+		}
+
+		foreach ($importPathList as $_dir) {
+			$this->parser->addImportPath($_dir);
+		}
 	}
 
 	protected function _onWrite($data) {
@@ -17,9 +30,8 @@ class Scss extends DataPipe {
 		$cssFile = str_replace('.less', '.css', $cssFile);
 		$file = new vfile( $cssFile );
 
-		$this->parser->parseFile( $data->getPathname() );
 		$file->setContents(
-			$this->parser->compile()
+			$this->parser->compile( file_get_contents($data->getPathname()) )
 		);
 		$this->push($file);
 	}
